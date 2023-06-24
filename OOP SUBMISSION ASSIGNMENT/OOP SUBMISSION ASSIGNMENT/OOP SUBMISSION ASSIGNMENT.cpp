2a7ffdf2253fce2593c37a49bfbb4710388c73f5
Zella Stewart <125938574+zeeebs@users.noreply.github.com>
2023-06-24 11:24:52 -0700

    OOP SUBMISSION ASSIGNMENT

--- OOP SUBMISSION ASSIGNMENT/OOP SUBMISSION ASSIGNMENT/OOP SUBMISSION ASSIGNMENT.cpp
+++ OOP SUBMISSION ASSIGNMENT/OOP SUBMISSION ASSIGNMENT/OOP SUBMISSION ASSIGNMENT.cpp
@@ -2,19 +2,38 @@
 //
 
 #include <iostream>
+using namespace std;
+
+//Create the exact class shown
+class Dog {
+public:
+	string Breed;
+	string Color;
+	string Height;
+	string Weight;
+	void Shake() {
+		cout << "shake \n";
+	}
+	void Sit() {
+		cout << "sit \n";
+	}
+	void LayDown() {
+		cout << "Lay Down \n";
+	}
+};
+//Create the instance of the class (object), and have it inherit all properties and methods.
+class Hound: public Dog {
+public:
+	string Breed = "Hound";
+	string Color = "Brown";
+	string Height = "2 feet";
+	string Weight = "60 pounds";
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Hound hound;
+	hound.Shake();
+	hound.Sit();
+	hound.LayDown();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
